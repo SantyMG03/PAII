fc8af035e85110f6992cc702691c4e3e734f984c
SantyMG03 <santymorenog2003@gmail.com>
2025-02-18 13:09:34 +0100

    Modificado ejercicio 3

--- Scala/src/main/scala/EjerciciosBasicos/Ejercicio3.scala
+++ Scala/src/main/scala/EjerciciosBasicos/Ejercicio3.scala
@@ -10,6 +10,6 @@ def esPalindromo(str: String): Boolean =
   print("Ingrese una cadena para verificar si es palíndromo: ")
   val entrada = StdIn.readLine()
   if esPalindromo(entrada) then
-    println("La cadena es un palíndromo.")
+    println("Es un palíndromo.")
   else
-    println("La cadena no es un palíndromo.")
+    println("No es un palíndromo.")
