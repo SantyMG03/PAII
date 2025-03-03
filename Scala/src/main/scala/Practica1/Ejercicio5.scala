765be614b12888ae3db2609b9b8f6b50588443dd
SantyMG03 <santymorenog2003@gmail.com>
2025-03-03 23:00:48 +0100

    Agregado prac1 ej 5

---
+++ Scala/src/main/scala/Practica1/Ejercicio5.scala
@@ -0,0 +1,17 @@
+package Practica1
+
+def filter[A](l: List[A], f: A => Boolean): List[A] = {
+  def func(remain: List[A], acc: List[A]): List[A] = {
+    remain match
+      case Nil => acc.reverse
+      case head :: tail if f(head) => func(tail, head :: acc)
+      case _ :: tail => func(tail, acc)
+  }
+  func(l, Nil)
+}
+
+@main def testFilter(): Unit = {
+  val l = List(1, 2, 3)
+  val fil = filter(l, _ % 2 != 0)
+  println(s"Filtered (odd numbers): $fil")
+}
