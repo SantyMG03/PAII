084dce2df6d073754335232642f22820c3d1bc87
SantyMG03 <santymorenog2003@gmail.com>
2025-03-24 13:45:04 +0100

    Ejericicios hasta el 13

---
+++ Scala/src/main/scala/Practica3/Ejercicio11-15.scala
@@ -0,0 +1,55 @@
+package Practica3
+
+// ------ Ejercicio 11 ------
+
+@main
+def ex11(): Unit = {
+  val logs = List(
+    "ERROR: Null pointer exception",
+    "INFO: User logged in",
+    "ERROR: Out of memory",
+    "WARNING: Disk space low",
+    "INFO: File uploaded",
+    "ERROR: Database connection failed"
+  )
+
+  val count = logs.groupBy(_.split(":")(0)).view.mapValues(_.size).toMap
+  val err = logs.filter(_.startsWith("ERROR"))
+
+  println(count)
+  println(err)
+}
+
+// ------ Ejercicio 12 ------
+
+@main
+def ex12(): Unit = {
+  val sales = List(
+    ("Laptop", 2, 1000.0),
+    ("Mouse", 10, 15.0),
+    ("Keyboard", 5, 50.0),
+    ("Monitor", 3, 200.0),
+    ("USB Drive", 20, 5.0)
+  )
+
+  val gain = sales.foldRight(0.0)((t, acc) => acc + (t._3 * t._2))
+  println(gain)
+
+  val report = sales.foldRight(List.empty[(String, Double)])((t, acc) => (t._1, (t._2 * t._3)) :: acc)
+  println(report)
+}
+
+// ------ Ejercicio 13 ------
+
+@main
+def ex13(): Unit = {
+  val sentences = Set(
+    "Scala is a functional language",
+    "The power of functional programming is great",
+    "Functional programming is elegant"
+  )
+  val stopWords = Set("a", "the", "is", "of")
+
+  val words = sentences.flatMap(_.toLowerCase.split("\\s+")).diff(stopWords).toSet
+  println(words)
+}
