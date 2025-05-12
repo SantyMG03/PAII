16ba4f2b42a415cdb8af17e5e2ac940f2a5fdd9e
SantyMG03 <santymorenog2003@gmail.com>
2025-05-12 12:00:47 +0200

    Septiembre2020 completamente funcional

---
+++ Java/src/Septiembre2020_Semaforos/Curso.java
@@ -0,0 +1,82 @@
+package Septiembre2020_Semaforos;
+
+import java.util.concurrent.Semaphore;
+
+public class Curso {
+
+	//Numero maximo de alumnos cursando simultaneamente la parte de iniciacion
+	private final int MAX_ALUMNOS_INI = 10;
+	
+	//Numero de alumnos por grupo en la parte avanzada
+	private final int ALUMNOS_AV = 3;
+
+	private int alumIni = 0;
+	private int alumAva = 0;
+	private int worked = 0;
+	private Semaphore mutex = new Semaphore(1);
+	private Semaphore iniciacion = new Semaphore(1);
+	private Semaphore avanzado = new Semaphore(1);
+	private Semaphore working = new Semaphore(0);
+	
+	
+	//El alumno tendra que esperar si ya hay 10 alumnos cursando la parte de iniciacion
+	public void esperaPlazaIniciacion(int id) throws InterruptedException{
+		//Espera si ya hay 10 alumnos cursando esta parte
+		iniciacion.acquire();
+		mutex.acquire();
+		alumIni++;
+		//Mensaje a mostrar cuando el alumno pueda conectarse y cursar la parte de iniciacion
+		System.out.println("PARTE INICIACION: Alumno " + id + " cursa parte iniciacion");
+		if (alumIni < 10) iniciacion.release();
+		mutex.release();
+	}
+
+	//El alumno informa que ya ha terminado de cursar la parte de iniciacion
+	public void finIniciacion(int id) throws InterruptedException{
+		mutex.acquire();
+		//Mensaje a mostrar para indicar que el alumno ha terminado la parte de principiantes
+		System.out.println("PARTE INICIACION: Alumno " + id + " termina parte iniciacion");
+		alumIni--;
+		if (alumIni < 10) iniciacion.release();
+		mutex.release();
+		//Libera la conexion para que otro alumno pueda usarla
+	}
+	
+	/* El alumno tendra que esperar:
+	 *   - si ya hay un grupo realizando la parte avanzada
+	 *   - si todavia no estan los tres miembros del grupo conectados
+	 */
+	public void esperaPlazaAvanzado(int id) throws InterruptedException{
+		//Espera a que no haya otro grupo realizando esta parte
+		avanzado.acquire();
+		alumAva++;
+		//Mensaje a mostrar si el alumno tiene que esperar al resto de miembros en el grupo
+		System.out.println("PARTE AVANZADA: Alumno " + id + " espera a que haya " + ALUMNOS_AV + " alumnos");
+		if (alumAva == 3) working.release();
+		else avanzado.release();
+		//Espera a que haya tres alumnos conectados
+		working.acquire();
+		//Mensaje a mostrar cuando el alumno pueda empezar a cursar la parte avanzada
+		System.out.println("PARTE AVANZADA: Hay " + ALUMNOS_AV + " alumnos. Alumno " + id + " empieza el proyecto");
+		worked ++;
+	}
+	
+	/* El alumno:
+	 *   - informa que ya ha terminado de cursar la parte avanzada 
+	 *   - espera hasta que los tres miembros del grupo hayan terminado su parte 
+	 */ 
+	public void finAvanzado(int id) throws InterruptedException{
+		//Espera a que los 3 alumnos terminen su parte avanzada
+		if (worked < 3) {
+			working.release();
+			//Mensaje a mostrar si el alumno tiene que esperar a que los otros miembros del grupo terminen
+			System.out.println("PARTE AVANZADA: Alumno " +  id + " termina su parte del proyecto. Espera al resto");
+		} else if (worked == 3) {
+			//Mensaje a mostrar cuando los tres alumnos del grupo han terminado su parte
+			System.out.println("PARTE AVANZADA: LOS " + ALUMNOS_AV + " ALUMNOS HAN TERMINADO EL CURSO");
+			worked = 0;
+			alumAva = 0;
+			avanzado.release();
+		}
+	}
+}
