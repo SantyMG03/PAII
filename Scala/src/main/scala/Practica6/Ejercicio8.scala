15617352dc5c52a3ecf62c7fa2aafffeb03bce12
SantyMG03 <santymorenog2003@gmail.com>
2025-05-14 17:18:20 +0200

    Ejercicio 8 practica lock funciona

--- Scala/src/main/scala/Practica6/Ejercicio8.scala
+++ Scala/src/main/scala/Practica6/Ejercicio8.scala
@@ -8,25 +8,64 @@ object gestorAgua {
   //CS-Ox1: El oxígeno que quiere formar una molécula espera si ya hay un oxígeno
   //CS-Ox2: El oxígeno debe esperar a los otros dos átomos para formar la molécula
 
+  private var puertaH = true
+  private var puertaO = true
+  private var molecula = false
+  private var hEsperando = 0;
+  private var oEsperando = 0;
 
-  def hidrogeno(id: Int) =  {
-    //el oxigeno id quiere formar una molécula
+  def hidrogeno(id: Int) = synchronized {
+    while (!puertaH) wait()
 
     log(s"Hidrógeno $id quiere formar una molécula")
+    hEsperando += 1
 
-      //log(s"      Molécula formada!!!")
+    if (hEsperando == 2) puertaH = false
 
+    if (hEsperando + oEsperando < 3) {
+      while (!molecula) wait()
+    } else {
+      molecula = true
+      notifyAll()
+    }
+
+    log(s"Hidrógeno $id participa en la formación de la molécula")
+    hEsperando -= 1
+
+    if (oEsperando + hEsperando == 0) {
+      log("      ¡Molécula formada!")
+      molecula = false
+      puertaO = true
+      puertaH = true
+    }
+    notifyAll()
   }
 
 
-
-  def oxigeno(id: Int) =  {
-    //el oxigeno id quiere formar una molécula
+  def oxigeno(id: Int) = synchronized {
+    while (!puertaO) wait()
 
     log(s"Oxígeno $id quiere formar una molécula")
+    oEsperando += 1
+    puertaO = false
 
-      //log(s"      Molécula formada!!!")
+    if (hEsperando + oEsperando < 3) {
+      while (!molecula) wait()
+    } else {
+      molecula = true
+      notifyAll()
+    }
 
+    log(s"Oxígeno $id participa en la formación de la molécula")
+    oEsperando -= 1
+
+    if (oEsperando + hEsperando == 0) {
+      log("      ¡Molécula formada!")
+      molecula = false
+      puertaO = true
+      puertaH = true
+    }
+    notifyAll()
   }
 
 }
